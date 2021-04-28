caf7b130e1681d05992dc6cedb8bfb61f039708c
kevincassel <kc.lewagon@gmail.com>
2021-04-28 06:52:09 +0200

    add hotjar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,14 @@
 //= require rails-ujs
 //= require_tree .
+
+<!-- Hotjar Tracking Code for http://www.garden-manager-certif-kc.xyz -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2372973,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
